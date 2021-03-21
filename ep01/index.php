baf98c5ef6779af01fc8a0d9a9c99bd2b3dda932
Thiago Ribeiro <thiagor21@gmail.com>
2021-03-21 15:50:25 -0300

    Aula finalizada

---
+++ ep01/index.php
@@ -0,0 +1,5 @@
+<?php
+ require __DIR__."/vendor/autoload.php";
+
+ echo "<h1>Olá Mundo, Bem vindo ao PHP TIPS</h1><br>";
+ echo "<h1>Hello World Wellcome to PHP TIPS</h1>";
---
+++ ep01/vendor/composer/installed.php
@@ -0,0 +1,36 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd3ece36b32a4159b52b7d55d161d8b3aa3c17d02',
+    'dev-requirement' => true,
+    'name' => 'robsonvleite/phptips001',
+  ),
+  'versions' => 
+  array (
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.3.0',
+      'version' => '6.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4a08cf4cdd2c38d12ee2b9fa69e5d235f37a6dcb',
+      'dev-requirement' => false,
+    ),
+    'robsonvleite/phptips001' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd3ece36b32a4159b52b7d55d161d8b3aa3c17d02',
+      'dev-requirement' => false,
+    ),
+  ),
+);
