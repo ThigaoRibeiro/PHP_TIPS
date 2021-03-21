d3c42a5b5e8cea55cb8373588e3e785b2412a07f
Thiago Ribeiro <thiagor21@gmail.com>
2021-03-21 16:35:08 -0300

    Iniciando PHP TIPS 02
    Configurações iniciais do PHP TIPS 02

---
+++ ep02/vendor/composer/installed.php
@@ -0,0 +1,36 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'baf98c5ef6779af01fc8a0d9a9c99bd2b3dda932',
+    'dev-requirement' => true,
+    'name' => 'robsonvleite/phptips002',
+  ),
+  'versions' => 
+  array (
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '928012861d39b855c91485a0f1bd29d202c2fd08',
+      'dev-requirement' => false,
+    ),
+    'robsonvleite/phptips002' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'baf98c5ef6779af01fc8a0d9a9c99bd2b3dda932',
+      'dev-requirement' => false,
+    ),
+  ),
+);
